02546d5e74524f1cac54ac1753abcccb39562956
bertRC <sithalbert@ya.ru>
2019-11-06 22:02:36 +0300

    class realized

--- src/main/java/ru/itpark/util/SqlMappingException.java
+++ src/main/java/ru/itpark/util/SqlMappingException.java
@@ -1,4 +1,22 @@
 package ru.itpark.util;
 
-public class SqlMappingException {
+public class SqlMappingException extends RuntimeException {
+    public SqlMappingException() {
+    }
+
+    public SqlMappingException(String message) {
+        super(message);
+    }
+
+    public SqlMappingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public SqlMappingException(Throwable cause) {
+        super(cause);
+    }
+
+    public SqlMappingException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
