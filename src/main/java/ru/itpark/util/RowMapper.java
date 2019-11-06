180ba82896d21294275eb4584ae88a1061653436
bertRC <sithalbert@ya.ru>
2019-11-06 22:04:10 +0300

    interface realized

--- src/main/java/ru/itpark/util/RowMapper.java
+++ src/main/java/ru/itpark/util/RowMapper.java
@@ -1,4 +1,9 @@
 package ru.itpark.util;
 
-public interface RowMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+    T map(ResultSet resultSet) throws SQLException;
 }
