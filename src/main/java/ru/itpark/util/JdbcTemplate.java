2637ee35c0c9d3b4d5121efe261920aaa1789f4c
bertRC <sithalbert@ya.ru>
2019-11-06 22:15:38 +0300

    some initial options added

--- src/main/java/ru/itpark/util/JdbcTemplate.java
+++ src/main/java/ru/itpark/util/JdbcTemplate.java
@@ -1,4 +1,8 @@
 package ru.itpark.util;
 
 public class JdbcTemplate {
+    public static final String uploadPath = "UPLOAD_PATH";
+    public static final String resourceName = "java:/comp/env/jdbc/db";
+
+    private JdbcTemplate() {}
 }
