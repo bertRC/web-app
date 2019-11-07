278d2048081df8d98dfee4feff050131da451ec6
bertRC <sithalbert@ya.ru>
2019-11-07 17:40:52 +0300

    executeQuery method realized

--- src/main/java/ru/itpark/util/JdbcTemplate.java
+++ src/main/java/ru/itpark/util/JdbcTemplate.java
@@ -2,6 +2,8 @@ package ru.itpark.util;
 
 import javax.sql.DataSource;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
     private JdbcTemplate() {
@@ -10,10 +12,29 @@ public class JdbcTemplate {
     public static void execute(DataSource ds, String sql) throws SQLException {
         try (
                 var conn = ds.getConnection();
-//                var stmt = conn.createStatement();
                 var stmt = conn.prepareStatement(sql);
         ) {
             stmt.execute();
         }
+    }
+
+    public static <T> List<T> executeQuery(DataSource ds, String sql, ValueSetter setter, RowMapper<T> mapper) throws SQLException {
+        try (
+                var conn = ds.getConnection();
+                var stmt = conn.prepareStatement(sql);
+        ) {
+            setter.setValues(stmt);
+            try (var rs = stmt.executeQuery();) {
+                List<T> result = new ArrayList<>();
+                while (rs.next()) {
+                    result.add(mapper.map(rs));
+                }
+                return result;
+            }
+        }
+    }
+
+    public static <T> List<T> executeQuery(DataSource ds, String sql, RowMapper<T> mapper) throws SQLException {
+        return executeQuery(ds, sql, stmt -> {}, mapper);
     }
 }
